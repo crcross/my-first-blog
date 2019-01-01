b3a1c7980249cf14399fa5b1794d2aa3a17b375b
ccross <ccross@standardsit.com>
2019-01-01 16:58:48 -0600

    Retry

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def home(request):
+    return print('Not Available')
+
